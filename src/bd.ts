567776180f3901802335ac7318dea557d1656a63
Dhammabhushan9 <dhammwagh922004@gmail.com>
2024-12-15 13:14:02 +0530

    Add validation  to data base

--- src/bd.ts
+++ src/bd.ts
@@ -1,29 +1,78 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 
 const Schema =mongoose.Schema;
 const ObjectId=Schema.ObjectId;
 
 const User= new Schema({
     
-    username:{type:String},
-    password:{type:String}
+    username:{
+    type:String ,
+    require:true,
+    unique:true,
+    minLength:[3,"username should be minimum 3 character"],
+    maxLength:[10,"username should be less than 10 character"]
+},
+    password:{
+        type:String,
+        require:true,
+        minLength:[8,"passwaord should be minimum should be 8 characters"],
+        maxLength:[20,"maximum length od password should be w0 character"]
+    }
 
 })
 
+const contentType=['image,post,video']// content should be incerase
+
 const Content =new Schema({
-    link:{type:String},
-    type:{type:String,enum}, 
-    title:{type:String},
-    tag:{type:String},
-    userId:{type:String,ObjectId,ref:User}
+    link:{
+        type:String,
+        require:true
+    },
+    type:{
+        type:String,enum:contentType,
+        require:true
+     }, 
+    title:{
+        type:String,
+        require:true
+    },
+    tag:{
+        type:Types.ObjectId,
+         ref:'Tag',
+    },
+
+    userId:{
+        type:Types.ObjectId,
+        ref:'User',
+        }
+        
  
 })
 
 const Tag= new Schema({
-    title:{type:String}
+    title:{
+        type:String,
+        require:true
+    }
 })
 
 const Link= new Schema({
     hash:{type:String},
-    userId:{}
+    userId:{
+        type: Types.ObjectId,
+        ref:'User'
+    } 
 })
+
+
+const UserModel=mongoose.model('users',User);
+const ContentModel=mongoose.model('contents',Content);
+const TagModel= mongoose.model("tags",Tag);
+const LinkModel=mongoose.model("links",Link);
+
+module.exports={
+    UserModel,
+    ContentModel,
+    TagModel,
+    LinkModel
+}
