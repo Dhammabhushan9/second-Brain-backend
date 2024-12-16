aca637ffd5d5fb7a67f0368640fe4f7b7258c850
Dhammabhusahna waghmare <98685161+Dhammabhushan9@users.noreply.github.com>
2024-12-16 00:36:27 -0800

    Add auth middelware for user

---
+++ src/middelware.ts
@@ -0,0 +1,29 @@
+import express, { Request,Response,NextFunction } from "express";
+import jwt from 'jsonwebtoken'
+const JWT_PASSWORD="iamgonabehokage";
+
+export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers['token'] as string | undefined; // Cast token as string or undefined
+
+    if (!token) {
+        return res.status(400).json({ message: 'Token is required' });
+    }
+ 
+
+    try {
+        // Verify the token and decode the user data
+        const decodedUser = jwt.verify(token, JWT_PASSWORD) as { _id: string };
+       
+        if (decodedUser) {
+            //@ts-ignore
+            req.userId = decodedUser.id;
+          
+         
+            next();
+        } else {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid token'  });
+    }
+};
